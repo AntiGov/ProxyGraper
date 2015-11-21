d49b350201743f125c7c83ad633bac39cb8ba203
G66K <G66K@INBOX.COM>
2015-11-20 23:50:02 -0500

    commit.

---
+++ proxyoff.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+#Coded By G66K
+#Disable Proxy For Entire SYstem
+if [ $(id -u) -ne 0 ]; then
+  echo "This script must be run as root";
+  exit 1;
+fi
+
+gsettings set org.gnome.system.proxy mode 'none' ;
+
+grep PATH /etc/environment > pxy.t;
+cat pxy.t > /etc/environment;
+
+printf "" > /etc/apt/apt.conf.d/95proxies;
+
+rm -rf pxy.t;
